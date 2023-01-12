a94e9f5b6b3ec8821d1263b9b6ec1bee9b249673
Muhammad Muneeb <imuneebofficial@gmail.com>
2023-01-12 16:07:45 +0500

    start your first app using node / functions

---
+++ node-basic/app.js
@@ -0,0 +1,14 @@
+function sum(a,b){
+    return a+b;
+}
+
+
+// var let const
+
+// const recommended
+
+// if value will change over time then use 'let' otherwise use const
+
+const total = sum(10, 200);
+
+console.log(total);
