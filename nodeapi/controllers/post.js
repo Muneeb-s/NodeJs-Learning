5d8e15b9357120e0cd2e3ff53583dbf0e2c3aaa5
Muhammad Muneeb <imuneebofficial@gmail.com>
2023-01-14 20:02:23 +0500

    get all from database

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -3,17 +3,13 @@ const Post = require('../models/post')
 
 
 exports.getPosts = (req, res)=>{
-    res.json({
-        posts : [
-            {
-                title: 'First Post'
-            },
-            {
-                title: 'Second Post'
-            }
-
-        ]
+    const posts = Post.find().select("_id title body") // space separated props to select
+    .then(posts => {
+        res.json({
+            posts
+        })
     })
+    .catch(err => console.log(err));
 }
 
 exports.createPost = (req, res) => {
@@ -33,7 +29,7 @@ exports.createPost = (req, res) => {
     // })
     post.save()
     .then(result => {
-        res.status(200).json({
+        res.json({
             post:result
         })
     })
